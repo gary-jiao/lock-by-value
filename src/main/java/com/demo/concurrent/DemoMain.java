b58724c81c3a650bf64dba583937c6d686f33047
gary.jiao <gary.jiao@medtronic.com>
2016-11-02 14:55:13 +0800

    optimized code

--- src/main/java/com/demo/concurrent/DemoMain.java
+++ src/main/java/com/demo/concurrent/DemoMain.java
@@ -4,43 +4,113 @@ import java.time.Clock;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import com.demo.concurrent.KeyLocker.KeyLockerThreadWorker;
+import com.demo.concurrent.ParallelWithDifferentKeyExecutor.DifferentKeyThreadWorker;
 
 public class DemoMain {
 	
 	public static void main(String[] args) throws Exception {
-		test();
+		test2();
 	}
 	
-	private static void test() {
+	private static void test2() throws Exception {
+		int m = 1;
+		List<User> userList = Stream.of(
+									new User(m++, "Hello", "111"),
+									new User(m++, "Hello1", "222"),
+									new User(m++, "Hello2", "222"),
+									new User(m++, "Hello3", "222"),
+									new User(m++, "Hello4", "111"),
+									new User(m++, "Hello5", "222"),
+									new User(m++, "Hello6", "222"),
+									new User(m++, "Hello7", "111")
+								).collect(Collectors.toList());
+		
+		ParallelWithDifferentKeyExecutor<Integer, User> kl = new ParallelWithDifferentKeyExecutor<>(5);
+		kl.addWorkers(userList, (user) -> user.getMobile(), (user) -> {
+			if (user.getId() == 5) throw new RuntimeException("test error");
+			return user.getId();
+		});
+		for (Future<Integer> future : kl.getResults()) {
+			try { 
+				System.out.println(future.get());
+			} catch (ExecutionException ex) {
+				ex.printStackTrace();
+				//如果希望遇到错误就取消所有任务，则可以执行以下注释掉的代码
+//				for(Future f : kl.getResults()){
+//		            f.cancel(true);
+//		        }
+//		        kl.getResults().clear();
+			}
+		}
+	}
+	
+	static class User {
+		private int id;
+		private String mobile;
+		private String username;
+		
+		public User(int id, String username, String mobile) {
+			this.id = id;
+			this.username = username;
+			this.mobile = mobile;
+		}
+		
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getMobile() {
+			return mobile;
+		}
+		public void setMobile(String mobile) {
+			this.mobile = mobile;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		
+	}
+	
+	private static <T> void test() {
 		List<String> phoneList = new ArrayList<>();
 		phoneList.add("13900000000");
 		phoneList.add("13911111111");
+		phoneList.add("13911111111");
+		phoneList.add("13900000000");
+		phoneList.add("13900000000");
+		phoneList.add("13900000000");
+		phoneList.add("13900000000");
+		phoneList.add("13900000000");
+		phoneList.add("13911111111");
+		phoneList.add("13900000000");
 //		phoneList.add("13922222222");
 //		phoneList.add("13933333333");
 //		phoneList.add("13944444444");
 //		phoneList.add("13955555555");
-		KeyLocker kl = KeyLocker.getInstance(5);
-		kl.start();
+		ParallelWithDifferentKeyExecutor<String, String> kl = new ParallelWithDifferentKeyExecutor<>(5);
 		
-		for (int i = 0; i < 10; i++) {
-			new Thread() {
-				public void run() {
-//					waitThread(5);
-					for (String phone : phoneList) {
-//						waitThread(2);
-						kl.addWorker(new KeyLockerThreadWorker(phone) {
-							@Override
-							protected void runWork() {
-								System.out.println(Thread.currentThread().getName() + " , " + Clock.systemUTC().millis() + " : working for , " + phone);
-								waitFixThread(2);
-							}
-						});
-					}
+		for (String phone : phoneList) {
+//			waitThread(2);
+			kl.addWorker(new DifferentKeyThreadWorker<String, String>(phone, phone, (phone1) -> {
+					System.out.println(Thread.currentThread().getName() + " , " + Clock.systemUTC().millis() + " : working for , " + phone1);
+					waitFixThread(2);
+					return Thread.currentThread().getName() + " , " + Clock.systemUTC().millis() + " : working for , " + phone1;
 				}
-			}.start();
+			){});
 		}
+		kl.getResults();
 	}
 	
 	private static void waitThread(int max) {
---
+++ src/main/java/com/demo/concurrent/ParallelWithDifferentKeyExecutor.java
@@ -0,0 +1,154 @@
+package com.demo.concurrent;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+
+/**
+ * <pre>
+ * Java正常的并发操作一般都是限制并发数量，如果是希望对某个值进行并发限制，目前没有找到已有的方法。
+ * 所以实现以下代码，根据传入主键的值进行判断，如果传入的值已经在运行中，则进行等待，否则启动运行。
+ * 也就是对于不同的键值，是处于并发操作的，但对于相同键值，是需要串行处理的
+ * </pre>
+ * 
+ * @author gary
+ *
+ */
+public class ParallelWithDifferentKeyExecutor<T, M> {
+	
+	private static Logger logger = LoggerFactory.getLogger(ParallelWithDifferentKeyExecutor.class);
+	
+	private volatile static ConcurrentMap<String, Object> workList = new ConcurrentHashMap<>();
+	
+	private ThreadPoolExecutor executor = null;
+	private Queue<DifferentKeyThreadWorker<T, M>> keyList = new ConcurrentLinkedQueue<>();
+	private Lock lock = new ReentrantLock();
+	private Boolean isRunning = false;
+	private List<Future<T>> resultList = new ArrayList<>();
+	
+	public ParallelWithDifferentKeyExecutor() {
+		//如果不指定线程数量，就默认取CPU的核数
+		this(Runtime.getRuntime().availableProcessors());
+	}
+
+	public ParallelWithDifferentKeyExecutor(int threadCount) {
+		this.executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadCount);
+	}
+	
+	public ParallelWithDifferentKeyExecutor<T, M> addWorkers(List<M> dataList, KeyMaker<M> maker, ThreadWorker<T, M> threadWorker) {
+		keyList.addAll(dataList.parallelStream().map(data -> new DifferentKeyThreadWorker<T, M>(maker.toKey(data), data, threadWorker){}).collect(Collectors.toList()));
+		return this;
+	}
+
+	/**
+	 * @param worker
+	 * @return
+	 */
+	public ParallelWithDifferentKeyExecutor<T, M> addWorker(DifferentKeyThreadWorker<T, M> worker) {
+		Assert.notNull(worker.getKey(), "The key cannot be null");
+		Assert.hasLength(worker.getKey(), "Key must not be empty");
+		keyList.add(worker);
+		return this;
+	}
+
+	protected ParallelWithDifferentKeyExecutor<T, M> run() {
+		try {
+			lock.lock();
+			if (isRunning) return this;
+			isRunning = true;
+		} finally {
+			lock.unlock();
+		}
+		for (;;) {
+			Iterator<DifferentKeyThreadWorker<T, M>> ite = keyList.iterator();
+			while (ite.hasNext()) {
+				DifferentKeyThreadWorker<T, M> data = ite.next();
+
+				lock.lock();
+				if (!workList.containsKey(data.getKey())) {
+					workList.putIfAbsent(data.getKey(), data);
+					resultList.add(executor.submit(data));
+					ite.remove();
+				}
+				lock.unlock();
+			}
+			if (keyList.isEmpty()) break;
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+			}
+		}
+		isRunning = false;
+		return this;
+	}
+	
+	public List<Future<T>> getResults() {
+		logger.info("The size of keyList is: " + keyList.size());
+		if (!keyList.isEmpty()) run();
+		while (!keyList.isEmpty()) ;
+		executor.shutdown();
+		return this.resultList;
+	}
+
+	public static abstract class DifferentKeyThreadWorker<T, M> implements Callable<T> {
+
+		private String key;
+		private M data;
+		private ThreadWorker<T, M> worker;
+
+		public DifferentKeyThreadWorker(String key, M data, ThreadWorker<T, M> worker) {
+			this.key = key;
+			this.data = data;
+			this.worker = worker;
+		}
+
+		public String getKey() {
+			return key;
+		}
+		
+		public M getData() {
+			return this.data;
+		}
+		
+		public T call() throws Exception {
+			try {
+				return worker.runWork(data);
+			} finally {
+				releaseWork();
+			}
+		}
+
+		public void releaseWork() {
+			workList.remove(key);
+		}
+
+		@Override
+		public String toString() {
+			return "KeyLockerThreadWorker [key=" + key + ", data=" + data + ", worker=" + worker + "]";
+		}
+
+	}
+	
+	public interface ThreadWorker<T, M> {
+		T runWork(M data);
+	}
+	
+	public interface KeyMaker<M> {
+		String toKey(M object);
+	}
+}
